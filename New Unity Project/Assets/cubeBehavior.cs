a32d2a6524f15765804ac6ef7b2d3f94b71d37e7
Dylan Nanjad <dnanjad00@gmail.com>
2019-09-11 22:46:53 -0400

    wall assignment in class

---
+++ New Unity Project/Assets/cubeBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeBehavior : MonoBehaviour
+{
+    public float decimalNumber;
+        
+    void Start()
+    {
+        decimalNumber = 1.11f;
+    }
+
+    void Update()
+    {
+        MoveMyObjectAlongAVector(this.transform, Vector3.up * Time.deltaTime);
+    }
+         float GiveMeTheSum(float numberA, float numberB)
+    { float numberSum = numberA + numberB;
+    return numberSum
+}
+  //  void MoveMyObjectAlongAVector(Transform theTransformThatNeedToBeMoved, Vector3)
